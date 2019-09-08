6657d18f7689cca4de07187fd714e4c4b7512258
avmepy <valentyn.kofanov@knu.ua>
2019-09-08 17:30:43 +0300

    create create T21.2

---
+++ regular/T21.2/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*-encoding: utf-8-*-
+# author: Valentyn Kofanov
+
+
+import re
+
+def main(filename):
+
+    with open(filename, 'r') as f:
+        st = f.read()
+
+    return re.split(r' ', st)
+
+
+if __name__ == '__main__':
+    print(main('test.txt'))
+
