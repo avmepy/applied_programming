7d43ae3712bd500956d879d9bdee0f24bf34f8e3
avmepy <valentyn.kofanov@knu.ua>
2019-09-15 14:39:50 +0300

    update

---
+++ hw/T21(regular)/ind/T21.6/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*-encoding: utf-8-*-
+# author: Valentyn Kofanov
+
+import re
+
+
+def main(filename):
+
+    res = []
+    patt = re.compile(r'[\w.]+@([\w-]+\.)+[\w-]{2,4}')
+    with open(filename, 'r') as fin:
+        for lines in fin:
+            res += list(map(lambda x: x.group(), patt.finditer(lines)))
+
+
+    return res
+
+if __name__ == '__main__':
+    for mail in main('input.txt'):
+        print(mail)
