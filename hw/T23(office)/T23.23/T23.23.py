212ac8c4c1e09715794dabaddb6d834eb4ce9608
avmepy <valentyn.kofanov@knu.ua>
2019-11-20 19:15:22 +0200

    update

--- hw/T23(office)/T23.23/T23.23.py
+++ hw/T23(office)/T23.23/T23.23.py
@@ -8,28 +8,25 @@ from docx.shared import RGBColor
 
 
 
-def main(filename_input, filename_output, c1=RGBColor(0, 0, 0), c2=RGBColor(255, 0, 0)):
+
+
+def main(filename_input, filename_output, c1=RGBColor(255, 0, 0), c2=RGBColor(45, 50, 0)):
     doc1 = docx.Document(filename_input)
-
-    run_list = [[run] for par in doc1.paragraphs for run in par.runs]
-
-    run_list_res = []
-
-    for par in run_list:
-        tmp = []
-        for run in par:
-            if c1  == run.textFormat.color.rgb == c1:
-                run.textFormat.color.rgb = c2
-            tmp.append(run)
-        run_list_res.append(tmp)
-
     doc2 = docx.Document()
 
-    for par in run_list_res:
-        doc2.add_paragraph(' '.join([run.text for run in par]))
+    for paragraph in list(doc1.paragraphs):
+        p = doc2.add_paragraph()
+        for i in range(len(paragraph.runs)):
+            run = paragraph.runs[i]
+            if run.font.color.rgb == c1:
+                run.font.color.rgb = c2
+                print(1)
+
+            r = p.add_run()
+            r.text = run.text
+            r.font.color.rgb = run.font.color.rgb
 
     doc2.save(filename_output)
 
-
 if __name__ == '__main__':
-    main('test.docx', 'res.docx')
+    main('res.docx', 'test.docx')
